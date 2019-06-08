7cb6fb9e51b412f8aecc2ba7151ae3a0cacb32e1
Honghao Zeng <nat@nat.moe>
2019-06-07 22:35:38 -0400

    proto: message: keepalive impl

--- protocol/bgp-keepalive-message.cc
+++ protocol/bgp-keepalive-message.cc
@@ -6,12 +6,19 @@ namespace bgpfsm {
 
 BgpKeepaliveMessage::BgpKeepaliveMessage() {}
 
-ssize_t BgpKeepaliveMessage::parse (const uint8_t *from, size_t msg_sz) {
+ssize_t BgpKeepaliveMessage::parse(const uint8_t *from, size_t msg_sz) {
+    if (msg_sz != 0) return -1; // KEEPALIVE can't have message body
+    return 0;
 
 }
 
 ssize_t BgpKeepaliveMessage::write(uint8_t *to, size_t msg_sz) const {
-    
+    return 0;
 }
+
+uint8_t BgpKeepaliveMessage::getErrorCode() const { return 0; }
+uint8_t BgpKeepaliveMessage::getErrorSubCode() const { return 0; }
+const uint8_t* BgpKeepaliveMessage::getError() const { return NULL; }
+size_t BgpKeepaliveMessage::getErrorLength() const { return 0; }
 
 }
