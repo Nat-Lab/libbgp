3e672094b83f29b36cc935bdb79d8151eb0b4f5e
Honghao Zeng <nat@nat.moe>
2019-06-08 17:18:26 -0400

    proto: msg: keepalive errors

--- protocol/bgp-keepalive-message.cc
+++ protocol/bgp-keepalive-message.cc
@@ -1,24 +1,45 @@
 #include "bgp-keepalive-message.h"
+#include "bgp-errcode.h"
 #include "bgp-error.h"
 #include <unistd.h>
 
 namespace bgpfsm {
 
-BgpKeepaliveMessage::BgpKeepaliveMessage() {}
+BgpKeepaliveMessage::BgpKeepaliveMessage() {
+    err_data = 0;
+}
 
 ssize_t BgpKeepaliveMessage::parse(const uint8_t *from, size_t msg_sz) {
-    if (msg_sz != 0) return -1; // KEEPALIVE can't have message body
-    return 0;
+    if (msg_sz != 0) {
+        err_data = msg_sz + 19;
+        return -1; // KEEPALIVE can't have message body
+    }
 
+    return 0;
 }
 
 ssize_t BgpKeepaliveMessage::write(uint8_t *to, size_t msg_sz) const {
     return 0;
 }
 
-uint8_t BgpKeepaliveMessage::getErrorCode() const { return 0; }
-uint8_t BgpKeepaliveMessage::getErrorSubCode() const { return 0; }
-const uint8_t* BgpKeepaliveMessage::getError() const { return NULL; }
-size_t BgpKeepaliveMessage::getErrorLength() const { return 0; }
+uint8_t BgpKeepaliveMessage::getErrorCode() const {
+    if (err_data > 0) return E_HEADER;
+    return 0;
+}
+
+uint8_t BgpKeepaliveMessage::getErrorSubCode() const { 
+    if (err_data > 0) return E_LENGTH;
+    return 0;
+}
+
+const uint8_t* BgpKeepaliveMessage::getError() const { 
+    if (err_data > 0) return &err_data;
+    return NULL;
+}
+
+size_t BgpKeepaliveMessage::getErrorLength() const { 
+    if (err_data > 0) return 1;
+    return 0;
+}
 
 }
--- protocol/bgp-keepalive-message.h
+++ protocol/bgp-keepalive-message.h
@@ -17,6 +17,9 @@ public:
     uint8_t getErrorSubCode() const;
     const uint8_t* getError() const;
     size_t getErrorLength() const;
+
+private:
+    uint8_t err_data;
 };
 
 }
