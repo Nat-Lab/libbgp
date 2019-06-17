63d354735d073b5635a90ece63721fd5835d9652
Honghao Zeng <nat@nat.moe>
2019-06-17 03:01:37 -0400

    proto: msg: keepal: use common seterr

--- protocol/bgp-keepalive-message.cc
+++ protocol/bgp-keepalive-message.cc
@@ -7,12 +7,12 @@ namespace bgpfsm {
 
 BgpKeepaliveMessage::BgpKeepaliveMessage() {
     type = KEEPALIVE;
-    err_data = 0;
 }
 
 ssize_t BgpKeepaliveMessage::parse(const uint8_t *from, size_t msg_sz) {
     if (msg_sz != 0) {
-        err_data = msg_sz + 19;
+        size_t err_len = msg_sz + 19;
+        setError(E_HEADER, E_LENGTH, (uint8_t *) &err_len, 1);
         return -1; // KEEPALIVE can't have message body
     }
 
@@ -20,26 +20,6 @@ ssize_t BgpKeepaliveMessage::parse(const uint8_t *from, size_t msg_sz) {
 }
 
 ssize_t BgpKeepaliveMessage::write(uint8_t *to, size_t msg_sz) const {
-    return 0;
-}
-
-uint8_t BgpKeepaliveMessage::getErrorCode() const {
-    if (err_data > 0) return E_HEADER;
-    return 0;
-}
-
-uint8_t BgpKeepaliveMessage::getErrorSubCode() const { 
-    if (err_data > 0) return E_LENGTH;
-    return 0;
-}
-
-const uint8_t* BgpKeepaliveMessage::getError() const { 
-    if (err_data > 0) return &err_data;
-    return NULL;
-}
-
-size_t BgpKeepaliveMessage::getErrorLength() const { 
-    if (err_data > 0) return 1;
     return 0;
 }
 
--- protocol/bgp-keepalive-message.h
+++ protocol/bgp-keepalive-message.h
@@ -12,14 +12,6 @@ public:
 
     ssize_t parse(const uint8_t *from, size_t msg_sz);
     ssize_t write(uint8_t *to, size_t buf_sz) const;
-
-    uint8_t getErrorCode() const;
-    uint8_t getErrorSubCode() const;
-    const uint8_t* getError() const;
-    size_t getErrorLength() const;
-
-private:
-    uint8_t err_data;
 };
 
 }
