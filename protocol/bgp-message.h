6318a4e0a81d009f83de700f91c1223ca8dabbcc
Honghao Zeng <nat@nat.moe>
2019-07-01 18:51:25 -0400

    bgp-msg: fixed msg type code

--- protocol/bgp-message.h
+++ protocol/bgp-message.h
@@ -9,8 +9,8 @@ namespace bgpfsm {
 enum BgpMessageType {
     OPEN = 1,
     UPDATE = 2,
-    KEEPALIVE = 3,
-    NOTIFICATION = 4
+    NOTIFICATION = 3,
+    KEEPALIVE = 4
 };
 
 class BgpMessage : public Serializable {
