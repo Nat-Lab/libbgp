e79ab33dd64dcd349e680d58fb2b71d64f97107f
Honghao Zeng <nat@nat.moe>
2019-07-01 11:05:26 -0400

    bgp-capability: make serializable

--- protocol/bgp-capability.cc
+++ protocol/bgp-capability.cc
@@ -1,20 +1,135 @@
 #include "bgp-capability.h"
+#include "bgp-errcode.h"
+#include "bgp-error.h"
+#include "value-op.h"
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+#include <arpa/inet.h>
 
 namespace bgpfsm {
 
-BgpCapabilityUnknow::BgpCapabilityUnknow(const uint8_t *buffer, uint8_t len) {
-    length = len;
+BgpCapability::BgpCapability() {
+    length = 0;
+}
 
-    if (len > 0) {
-        value = (uint8_t *) malloc(len);
-        memcpy(value, buffer, len);
+ssize_t BgpCapability::parseHeader(const uint8_t *from, size_t msg_sz) {
+    if (msg_sz < 2) {
+        setError(E_OPEN, E_UNSPEC_OPEN, NULL, 0);
+        _bgp_error("BgpCapability::parseHeader: unexpected end of capability.\n");
+        return -1;
     }
+
+    code = getValue<uint8_t>(&from);
+    length = getValue<uint8_t>(&from);
+
+    if (length + 2 > msg_sz) {
+        setError(E_OPEN, E_UNSPEC_OPEN, NULL, 0);
+        _bgp_error("BgpCapability::parseHeader: capability size exceed capabilities list.\n");
+        return -1;
+    }
+
+    return 2;
+}
+
+BgpCapability4BytesAsn::BgpCapability4BytesAsn() {
+    my_asn = 0;
+    code = ASN_4B;
+}
+
+ssize_t BgpCapability4BytesAsn::doPrint(size_t indent, uint8_t **to, size_t *buf_sz) const {
+    ssize_t written;
+    written += _print(indent, to, buf_sz, "FourOctetAsnCapability {\n");
+    indent++; {
+        written += _print(indent, to, buf_sz, "Code { %d }\n", code);
+        written += _print(indent, to, buf_sz, "MyAsn { %d }\n", my_asn);
+    }; indent--;
+    written += _print(indent, to, buf_sz, "}\n");
+
+    return written;
+}
+
+ssize_t BgpCapability4BytesAsn::parse(const uint8_t *from, size_t msg_sz) {
+    ssize_t hdr_len = parseHeader(from, msg_sz);
+
+    assert(code == ASN_4B);
+
+    if (hdr_len < 0) return hdr_len;
+    if (length != 4) {
+        setError(E_OPEN, E_UNSPEC_OPEN, NULL, 0);
+        _bgp_error("BgpCapability4BytesAsn::parse: bad length field (saw %d, want 4).\n", length);
+        return -1;
+    }
+
+    my_asn = ntohl(getValue<uint32_t>(&from));
+
+    return hdr_len + 4;
+}
+
+ssize_t BgpCapability4BytesAsn::write(uint8_t *to, size_t buf_sz) const {
+    if (buf_sz < 6) {
+        _bgp_error("BgpCapability4BytesAsn::write: dest buffer too small.\n");
+        return -1;
+    }
+
+    uint8_t *buffer = to;
+    putValue<uint8_t>(&buffer, ASN_4B);
+    putValue<uint8_t>(&buffer, 4);
+    putValue<uint32_t>(&buffer, htonl(my_asn));
+
+    return 6;
+}
+
+BgpCapabilityUnknow::BgpCapabilityUnknow() {
+    value = NULL;
 }
 
 BgpCapabilityUnknow::~BgpCapabilityUnknow() {
-    if (length > 0) free(value);
+    if (length > 0 && value != NULL) free(value);
 }
+
+ssize_t BgpCapabilityUnknow::doPrint(size_t indent, uint8_t **to, size_t *buf_sz) const {
+    ssize_t written;
+    written += _print(indent, to, buf_sz, "UnknowCapability {\n");
+    indent++; {
+        written += _print(indent, to, buf_sz, "Code { %d }\n", code);
+    }; indent--;
+    written += _print(indent, to, buf_sz, "}\n");
+
+    return written;
+}
+
+ssize_t BgpCapabilityUnknow::parse(const uint8_t *from, size_t msg_sz) {
+    ssize_t hdr_len = parseHeader(from, msg_sz);
+
+    if (hdr_len < 0) return hdr_len;
+
+    value = (uint8_t *) malloc(length);
+    memcpy(value, from + hdr_len, length);
+
+    return hdr_len + length;
+}
+
+ssize_t BgpCapabilityUnknow::write(uint8_t *to, size_t buf_sz) const {
+    if (buf_sz < length + 2) {
+        _bgp_error("BgpCapabilityUnknow::write: dest buffer too small.\n");
+        return -1;
+    }
+
+    uint8_t *buffer = to;
+    putValue<uint8_t>(&buffer, code);
+    putValue<uint8_t>(&buffer, length);
+    if (length == 0) return 2;
+    if (value == NULL) {
+        _bgp_error("BgpCapabilityUnknow: missing value pointer.\n");
+        return -1;
+    }
+    memcpy(buffer, value, length);
+
+    return length + 2;
+}
+
+
+
 
 }
--- protocol/bgp-capability.h
+++ protocol/bgp-capability.h
@@ -1,5 +1,6 @@
 #ifndef BGP_CAP_H_
 #define BGP_CAP_H_
+#include "serializable.h"
 #include <stdint.h>
 
 namespace bgpfsm {
@@ -17,22 +18,42 @@ enum BgpCapabilityCode {
     ENHANCED_ROUTE_REFRESH = 70
 };
 
-class BgpCapability {
+class BgpCapability : public Serializable {
 public:
+    BgpCapability();
+
     uint8_t code;
-    uint8_t length;
     virtual ~BgpCapability() {}
+protected:
+
+    ssize_t parseHeader(const uint8_t *from, size_t msg_sz);
+
+    // length field is only used in parse to pass length to lower-level parser.
+    // length field is ignored when serializing.
+    uint8_t length;
 };
 
 class BgpCapability4BytesAsn : public BgpCapability {
 public:
+    BgpCapability4BytesAsn();
+
+    ssize_t doPrint(size_t indent, uint8_t **to, size_t *buf_sz) const;
+    ssize_t parse(const uint8_t *from, size_t msg_sz);
+    ssize_t write(uint8_t *to, size_t buf_sz) const;
+
     uint32_t my_asn;
 };
 
 class BgpCapabilityUnknow : public BgpCapability {
 public:
-    BgpCapabilityUnknow (const uint8_t *buffer, uint8_t len);
-    ~BgpCapabilityUnknow ();
+    BgpCapabilityUnknow();
+    ~BgpCapabilityUnknow();
+
+    ssize_t doPrint(size_t indent, uint8_t **to, size_t *buf_sz) const;
+    ssize_t parse(const uint8_t *from, size_t msg_sz);
+    ssize_t write(uint8_t *to, size_t buf_sz) const;
+
+private:
     uint8_t *value;
 };
 
