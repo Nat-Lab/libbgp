4b11eaa9b7d57885b4e73cca345991f57fcb8abf
Honghao Zeng <nat@nat.moe>
2019-07-02 20:03:28 -0400

    bgp-rib: implementation

---
+++ bgp-rib.cc
@@ -0,0 +1,109 @@
+#include "bgp-rib.h"
+
+namespace bgpfsm {
+
+BgpRibEntry::BgpRibEntry(Route r, uint32_t src, const std::vector<std::shared_ptr<BgpPathAttrib>> as) : route(r), attribs(as) {
+    src_router_id = src;
+}
+
+uint32_t BgpRibEntry::getMetric() const {
+    for (const std::shared_ptr<BgpPathAttrib> &attr : attribs) {
+        if (attr->type_code == AS_PATH) {
+            const BgpPathAttribAsPath &as_path = dynamic_cast<const BgpPathAttribAsPath &>(*attr);
+            for (const BgpAsPathSegment &seg : as_path.as_paths) {
+                if (seg.type == AS_SEQUENCE) return seg.value.size();
+            }
+        }
+    }
+
+    return 0;
+}
+
+bool BgpRib::insert(uint32_t src_router_id, const Route &route, const std::vector<std::shared_ptr<BgpPathAttrib>> &attrib) {
+    for (const BgpRibEntry &entry : rib) {
+        if (entry.route == route && entry.src_router_id == src_router_id) return false;
+    }
+    rib.push_back(BgpRibEntry(route, src_router_id, attrib));
+    return true;
+}
+
+ssize_t BgpRib::insert(uint32_t src_router_id, const std::vector<Route> &routes, const std::vector<std::shared_ptr<BgpPathAttrib>> &attrib) {
+    size_t inserted = 0;
+    for (const Route &r : routes) {
+        if (insert(src_router_id, r, attrib)) inserted++;
+    }
+    return inserted;
+}
+
+bool BgpRib::withdraw(uint32_t src_router_id, const Route &route) {
+    for (std::vector<BgpRibEntry>::const_iterator entry = rib.begin(); entry != rib.end(); entry++) {
+        if (entry->route == route && entry->src_router_id == src_router_id) {
+            rib.erase(entry);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+ssize_t BgpRib::discard(uint32_t src_router_id) {
+    size_t n_erased = 0;
+    for (std::vector<BgpRibEntry>::const_iterator entry = rib.begin(); entry != rib.end();) {
+        if (entry->src_router_id == src_router_id) {
+            rib.erase(entry);
+            n_erased++;
+        } else entry++;
+    }
+
+    return n_erased;
+}
+
+const BgpRibEntry* BgpRib::selectEntry(const BgpRibEntry *a, const BgpRibEntry *b) {
+    assert(!(a == NULL && b == NULL));
+
+    if (a == NULL) return b;
+    if (b == NULL) return a;
+
+    const Route &ra = a->route;
+    const Route &rb = b->route;
+
+    // a is more specific, use a
+    if (ra.getLength() > rb.getLength()) return a;
+
+    // a, b are same level of specific, check metric
+    if (ra.getLength() == rb.getLength()) {
+        // return the one with lower metric
+        return (a->getMetric() > b->getMetric()) ? b : a;
+    }
+
+    // b is more specific, use b
+    return b;
+}
+
+const BgpRibEntry* BgpRib::lookup(uint32_t dest) const {
+    const BgpRibEntry *selected_entry = NULL;
+
+    for (const BgpRibEntry &entry : rib) {
+        const Route &route = entry.route;
+        if (route.includes(dest)) 
+            selected_entry = selectEntry(&entry, selected_entry);
+    }
+
+    return selected_entry;
+}
+
+const BgpRibEntry* BgpRib::lookup(uint32_t src_router_id, uint32_t dest) const {
+    uint8_t max_length = 0;
+    const BgpRibEntry *selected_entry = NULL;
+
+    for (const BgpRibEntry &entry : rib) {
+        if (entry.src_router_id != src_router_id) continue;
+        const Route &route = entry.route;
+        if (route.includes(dest)) 
+            selected_entry = selectEntry(&entry, selected_entry);
+    }
+
+    return selected_entry;
+}
+
+}
--- bgp-rib.h
+++ bgp-rib.h
@@ -3,34 +3,50 @@
 #include <stdint.h>
 #include <vector>
 #include <memory>
+#include "route.h"
 #include "protocol/bgp-path-attrib.h"
 
 namespace bgpfsm {
 
-typedef struct BgpRibEntry {
+class BgpRibEntry {
+public:
+    BgpRibEntry (Route r, uint32_t src, const std::vector<std::shared_ptr<BgpPathAttrib>> attribs);
     Route route;
     uint32_t src_router_id;
 
     std::vector<std::shared_ptr<BgpPathAttrib>> attribs;
-} RibEntry;
+
+    // compute metric based on path attribute. (currently based on AS_PATH only)
+    uint32_t getMetric() const;
+};
 
 class BgpRib {
 public:
-    // insert a new route into RIB
-    int insert(uint32_t src_router_id, const Route &route, const std::vector<std::shared_ptr<BgpPathAttrib>> &attrib);
+    // insert a new route into RIB, return true if success.
+    bool insert(uint32_t src_router_id, const Route &route, const std::vector<std::shared_ptr<BgpPathAttrib>> &attrib);
 
-    // insert new routes into RIB
-    int insert(uint32_t src_router_id, const std::vector<Route> &route, const std::vector<std::shared_ptr<BgpPathAttrib>> &attrib);
+    // insert new routes into RIB, return number of routes inserted on success,
+    // -1 on error.
+    ssize_t insert(uint32_t src_router_id, const std::vector<Route> &routes, const std::vector<std::shared_ptr<BgpPathAttrib>> &attrib);
 
-    // remove a route from RIB
-    int withdraw(uint32_t src_router_id, const Route &route);
+    // remove a route from RIB, return true if route removed, false if not exist.
+    bool withdraw(uint32_t src_router_id, const Route &route);
 
-    // remove all routes from a peer
-    int discard(uint32_t src_router_id);
+    // remove all routes from a peer, return number of routes remvoed on success, 
+    // -1 on error.
+    ssize_t discard(uint32_t src_router_id);
+
+    // lookup in rib, return null if not found
+    const BgpRibEntry* lookup(uint32_t dest) const;
+
+    // scoped lookup in rib, return null if not found
+    const BgpRibEntry* lookup(uint32_t src_router_id, uint32_t dest) const;
 
     // get RIB
     const std::vector<BgpRibEntry> &get() const;
 private:
+
+    static const BgpRibEntry* selectEntry(const BgpRibEntry *a, const BgpRibEntry *b);
     std::vector<BgpRibEntry> rib;
 };
 
