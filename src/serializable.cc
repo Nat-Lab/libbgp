c9c709c98b05181ca82d17b5bbf613513d1c2406
Honghao Zeng <nat@nat.moe>
2019-07-04 18:36:07 -0400

    serializable: doc

--- src/serializable.cc
+++ src/serializable.cc
@@ -1,3 +1,13 @@
+/**
+ * @file serializable.cc
+ * @author Nato Morichika <nat@nat.moe>
+ * @brief The serializable base.
+ * @version 0.1
+ * @date 2019-07-04
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #include "serializable.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +16,10 @@
 
 namespace libbgp {
 
+/**
+ * @brief Construct a new Serializable:: Serializable object
+ * 
+ */
 Serializable::Serializable() {
     err_code = 0;
     err_subcode = 0;
@@ -13,14 +27,35 @@ Serializable::Serializable() {
     err_len = 0;
 }
 
+/**
+ * @brief Destroy the Serializable:: Serializable object
+ * 
+ */
 Serializable::~Serializable() {
     if (err_len > 0 && err_data != NULL) free(err_data);
 }
 
+/**
+ * @brief Check if error information available.
+ * 
+ * @return true information avaliable.
+ * @return false information not avaliable.
+ */
 bool Serializable::hasError() const {
     return err_len != 0;
 }
 
+/**
+ * @brief Set the error information.
+ * 
+ * Set the error information. The information is used by BgpFsm to send 
+ * `NOTIFICATION` message to the peer.
+ * 
+ * @param err The error code.
+ * @param suberr The error subcode.
+ * @param data The error data buffer.
+ * @param data_len The length of error data buffer.
+ */
 void Serializable::setError(uint8_t err, uint8_t suberr, const uint8_t *data, size_t data_len) {
     err_code = err;
     err_subcode = suberr;
@@ -50,18 +85,58 @@ size_t Serializable::getErrorLength() const {
     return err_len;
 }
 
+/**
+ * @brief Forward error information from other Serializable object.
+ * 
+ * @param other The other Serializable object.
+ */
 void Serializable::forwardParseError(const Serializable &other) {
     setError(other.getErrorCode(), other.getErrorSubCode(), other.getError(), other.getErrorLength());
 }
 
+/**
+ * @brief Print the Serializable object as human readable string.
+ * 
+ * @param to The pointer to the string buffer.
+ * @param buf_sz The length of string buffer.
+ * @return ssize_t Bytes written.
+ * @retval -1 Failed to print.
+ * @retval >= 0 Bytes written.
+ */
 ssize_t Serializable::print(uint8_t *to, size_t buf_sz) const {
     return doPrint(0, &to, &buf_sz);
 }
 
+/**
+ * @brief Print the Serializable object as human readable string, with 
+ * indentation.
+ * 
+ * @param indent indent level.
+ * @param to The pointer to the string buffer.
+ * @param buf_sz The length of string buffer.
+ * @return ssize_t Bytes written.
+ * @retval -1 Failed to print.
+ * @retval >= 0 Bytes written.
+ */
 ssize_t Serializable::print(size_t indent, uint8_t *to, size_t buf_sz) const {
     return doPrint(indent, &to, &buf_sz);
 }
 
+/**
+ * @brief Print helper.
+ * 
+ * Print helper prints string to buffer with printf format, increment the buffer
+ * pointer and decrease the available buffer pointer.
+ * 
+ * @param indent indent level.
+ * @param to The pointer to the pointer to the string buffer.
+ * @param buf_left The pointer to the counter of avaliable buffer space.
+ * @param format The printf format string.
+ * @param ... 
+ * @return ssize_t Bytes written.
+ * @retval -1 Failed to print.
+ * @retval >=0 Bytes written.
+ */
 ssize_t Serializable::_print(size_t indent, uint8_t **to, size_t *buf_left, const char* format, ...) {
     if (*buf_left <= indent * 4) return 0;
     for (size_t i = 0; i < indent; i++) {
--- src/serializable.h
+++ src/serializable.h
@@ -1,3 +1,13 @@
+/**
+ * @file serializable.h
+ * @author Nato Morichika <nat@nat.moe>
+ * @brief The serializable base.
+ * @version 0.1
+ * @date 2019-07-04
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #ifndef SERIALIZABLE_H_
 #define SERIALIZABLE_H_
 #include <stdint.h>
@@ -6,6 +16,10 @@
 
 namespace libbgp {
 
+/**
+ * @brief The serializable base class.
+ * 
+ */
 class Serializable {
 public:
     Serializable();
@@ -17,10 +31,29 @@ public:
     // print the Serializable object as human readable string, pre-indented.
     ssize_t print(size_t indent, uint8_t *to, size_t buf_sz) const;
 
-    // deserialize the Serializable object from buffer.
+    /**
+     * @brief Deserialize the object from buffer.
+     * 
+     * @param from The pointer to buffer.
+     * @param msg_sz The max read length of deserializer.
+     * @return ssize_t Bytes read.
+     * @retval -1 Deserialization failed. Error information might be avaliable 
+     * in errbuf (see bgp-error.h) and peer should be notified with 
+     * `getError*()`.
+     * @retval >=0 Bytes read.
+     */
     virtual ssize_t parse(const uint8_t *from, size_t msg_sz) = 0;
 
-    // serialize the Serializable object to buffer.
+    /**
+     * @brief Serialize the object and write to buffer.
+     * 
+     * @param to The pointer to buffer.
+     * @param buf_sz The max write size of serializer.
+     * @return ssize_t Btyes written.
+     * @retval -1 Serialization failed. Error information might be avaliable in 
+     * errbuf (see bgp-error.h)
+     * @retval >=0 Bytes written.
+     */
     virtual ssize_t write(uint8_t *to, size_t buf_sz) const = 0;
 
     bool hasError() const;
@@ -42,7 +75,16 @@ protected:
     // buffer pointer and buf_left after write. return bytes written
     static ssize_t _print(size_t indent, uint8_t **to, size_t *buf_left, const char* format, ...);
 
-    // print the object, indent it for indent times.
+    /**
+     * @brief Print implementation.
+     * 
+     * @param indent indent level.
+     * @param to The pointer to the pointer to the string buffer.
+     * @param buf_sz The pointer to the counter of avaliable buffer space.
+     * @return ssize_t ssize_t Bytes written.
+     * @retval -1 Failed to print.
+     * @retval >= 0 Bytes written.
+     */
     virtual ssize_t doPrint(size_t indent, uint8_t **to, size_t *buf_sz) const = 0;
 
     // utility function to error related values
