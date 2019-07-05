85dbfa9d6eb25e1ec2e88a8183a701fe754f5890
Honghao Zeng <nat@nat.moe>
2019-07-04 20:20:25 -0400

    value-op: doc

--- src/value-op.h
+++ src/value-op.h
@@ -1,3 +1,13 @@
+/**
+ * @file value-op.h
+ * @author Nato Morichika <nat@nat.moe>
+ * @brief Buffer operation helpers.
+ * @version 0.1
+ * @date 2019-07-04
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #ifndef VALUE_OP_H_
 #define VALUE_OP_H_
 #include <stdint.h>
@@ -6,7 +16,27 @@
 
 namespace libbgp {
 
+/**
+ * @brief Get value from buffer.
+ * 
+ * Read value from buffer pointer and move buffer pointer.
+ * 
+ * @tparam T Type of value.
+ * @param buffer Pointer to buffer.
+ * @return T The value.
+ */
 template <typename T> T getValue(const uint8_t **buffer);
+
+/**
+ * @brief Put value to buffer.
+ * 
+ * Write the value to buffer pointer and move the buffer pointer.
+ * 
+ * @tparam T Type of value.
+ * @param buffer Pointer to pointer to buffer.
+ * @param value Value to write.
+ * @return size_t Bytes written.
+ */
 template <typename T> size_t putValue(uint8_t **buffer, T value);
 
 template <typename T> T getValue(const uint8_t **buffer) {
