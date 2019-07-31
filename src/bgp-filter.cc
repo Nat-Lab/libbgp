67bf46e516cea8f0553bcfcdf956fafb3556965b
Maho Morichika <nat@magicnat.com>
2019-07-31 21:52:53 +0800

    bgp-filter: 6/4 merged, added more prefix tests, comm/asn match

---
+++ src/bgp-filter.cc
@@ -0,0 +1,209 @@
+/**
+ * @file bgp-filter.cc
+ * @author Nato Morichika <nat@nat.moe>
+ * @brief The BGP route filtering engine.
+ * @version 0.1
+ * @date 2019-07-31
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+#include <arpa/inet.h>
+#include "bgp-filter.h"
+#include "value-op.h"
+
+namespace libbgp {
+
+BgpFilterRule::~BgpFilterRule() {}
+
+/**
+ * @brief Construct a new BgpFilterRuleRoute object
+ * 
+ */
+BgpFilterRuleRoute::BgpFilterRuleRoute() {
+    filter_type = F_ROUTE;
+}
+
+/**
+ * @brief Destroy the BgpFilterRuleRoute object
+ * 
+ */
+BgpFilterRuleRoute::~BgpFilterRuleRoute() {}
+
+/**
+ * @brief Construct a new IPv4 route filtering object
+ * 
+ * @param op Action to take if the prefix matched.
+ * @param type Type of matching.
+ * @param prefix Prefix to match.
+ */
+BgpFilterRuleRoute4::BgpFilterRuleRoute4(BgpFilterOP op, BgpFilterRuleRouteMatchType type, const Prefix4 &prefix) {
+    this->op = op;
+    this->match_type = type;
+    this->prefix = prefix;
+}
+
+BgpFilterOP BgpFilterRuleRoute4::apply(const Prefix &prefix, __attribute__((unused)) const std::vector<std::shared_ptr<BgpPathAttrib>> &attribs) {
+    if (prefix.afi != IPV4) return NOP;
+    const Prefix4 &prefix_4 = dynamic_cast<const Prefix4 &>(prefix);
+    switch (match_type) {
+        case M_EQ: return prefix_4 == this->prefix ? op : NOP;
+        case M_NE: return prefix_4 != this->prefix ? op : NOP;
+        case M_GT: return prefix_4 != this->prefix && prefix_4.includes(this->prefix) ? op : NOP;
+        case M_LT: return prefix_4 != this->prefix && this->prefix.includes(prefix_4) ? op : NOP;
+        case M_GE: return prefix_4.includes(this->prefix) ? op : NOP;
+        case M_LE: return this->prefix.includes(prefix_4) ? op : NOP;
+    }
+
+    return NOP;
+}
+
+/**
+ * @brief Construct a new IPv6 route filtering object
+ * 
+ * @param op Action to take if the prefix matched.
+ * @param type Type of matching.
+ * @param prefix Prefix to match.
+ */
+BgpFilterRuleRoute6::BgpFilterRuleRoute6(BgpFilterOP op, BgpFilterRuleRouteMatchType type, const Prefix6 &prefix) {
+    this->op = op;
+    this->match_type = type;
+    this->prefix = prefix;
+}
+
+BgpFilterOP BgpFilterRuleRoute6::apply(const Prefix &prefix, __attribute__((unused)) const std::vector<std::shared_ptr<BgpPathAttrib>> &attribs) {
+    if (prefix.afi != IPV4) return NOP;
+    const Prefix6 &prefix_6 = dynamic_cast<const Prefix6 &>(prefix);
+    switch (match_type) {
+        case M_EQ: return prefix_6 == this->prefix ? op : NOP;
+        case M_NE: return prefix_6 != this->prefix ? op : NOP;
+        case M_GT: return prefix_6 != this->prefix && prefix_6.includes(this->prefix) ? op : NOP;
+        case M_LT: return prefix_6 != this->prefix && this->prefix.includes(prefix_6) ? op : NOP;
+        case M_GE: return prefix_6.includes(this->prefix) ? op : NOP;
+        case M_LE: return this->prefix.includes(prefix_6) ? op : NOP;
+    }
+
+    return NOP;
+}
+
+/**
+ * @brief Construct a new AS_PATH filtering object
+ * 
+ * @param op Action to take if the asn matched.
+ * @param type Type of matching.
+ * @param asn ASN to match.
+ */
+BgpFilterRuleAsPath::BgpFilterRuleAsPath(BgpFilterOP op, BgpFilterRuleAsPathMatchType type, uint32_t asn) {
+    this->op = op;
+    this->match_type = type;
+    this->asn = asn;
+}
+
+BgpFilterOP BgpFilterRuleAsPath::apply(__attribute__((unused)) const Prefix &prefix, const std::vector<std::shared_ptr<BgpPathAttrib>> &attribs) {
+    for (const std::shared_ptr<BgpPathAttrib> &attr : attribs) {
+        if (attr->type_code != AS_PATH) continue;
+        const BgpPathAttribAsPath &as_path = dynamic_cast<const BgpPathAttribAsPath &>(*attr);
+
+        for (const BgpAsPathSegment &as_seg : as_path.as_paths) {
+            if (as_seg.type != AS_SEQUENCE) continue;
+
+            if (match_type == M_FROM_ASN && asn == as_seg.value.back()) return op;
+            if (match_type == M_NOT_FROM_ASN && asn != as_seg.value.back()) return op;
+
+            for (uint32_t asn : as_seg.value) {
+                if (asn == this->asn && match_type == M_HAS_ASN) return op;
+            }
+
+        }
+
+        if (match_type == M_NOT_HAS_ASN) return op; 
+    }
+
+    return NOP;
+}
+
+/**
+ * @brief Construct a new COMMUNITY filtering object
+ * 
+ * @param op Action to take if community matched.
+ * @param type Type of matching.
+ * @param asn ASN part of the community. 'AAAA' in (AAAA:BBBB)
+ * @param keyword keyword part of the community. 'BBBB' in (AAAA:BBBB)
+ */
+BgpFilterRuleCommunity::BgpFilterRuleCommunity(BgpFilterOP op, BgpFilterRuleCommunityMatchType type, uint16_t asn, uint16_t keyword) {
+    this->op = op;
+    this->match_type = type;
+    uint8_t *community_ptr = (uint8_t *) &community;
+    putValue<uint16_t>(&community_ptr, htons(asn));
+    putValue<uint16_t>(&community_ptr, htons(keyword));
+}
+
+/**
+ * @brief  Construct a new COMMUNITY filtering object
+ * 
+ * @param op Action to take if community matched.
+ * @param type Type of matching.
+ * @param community Raw community value in host-byte order.
+ */
+BgpFilterRuleCommunity::BgpFilterRuleCommunity(BgpFilterOP op, BgpFilterRuleCommunityMatchType type, uint32_t community) {
+    this->op = op;
+    this->match_type = type;
+    this->community = htonl(community);
+}
+
+BgpFilterOP BgpFilterRuleCommunity::apply(__attribute__((unused)) const Prefix &prefix, const std::vector<std::shared_ptr<BgpPathAttrib>> &attribs) {
+    for (const std::shared_ptr<BgpPathAttrib> &attr : attribs) {
+        if (attr->type_code != COMMUNITY) continue;
+        const BgpPathAttribCommunity &community = dynamic_cast<const BgpPathAttribCommunity &>(*attr);
+
+        for (uint32_t community_val : community.communites) {
+            if (match_type == M_HAS_COMMUNITY && community_val == this->community) return op;
+        }
+
+        if (match_type == M_NOT_HAS_COMMUNITY) return op;
+    }
+
+    return NOP;
+}
+
+/**
+ * @brief Construct a new BgpFilterRules rules set.
+ * 
+ * Default action is ACCEPT.
+ * 
+ */
+BgpFilterRules::BgpFilterRules() {
+    default_op = ACCEPT;
+}
+
+/**
+ * @brief Construct a new BgpFilterRules rules set.
+ * 
+ * @param default_op Default action.
+ */
+BgpFilterRules::BgpFilterRules(BgpFilterOP default_op) {
+    this->default_op = default_op;
+}
+
+/**
+ * @brief Apply the rules set on a route.
+ * 
+ * @param prefix Route prefix.
+ * @param attribs Path attribues.
+ * @return BgpFilterOP Action to take.
+ */
+BgpFilterOP BgpFilterRules::apply(const Prefix &prefix, const std::vector<std::shared_ptr<BgpPathAttrib>> &attribs) {
+    if (rules.size() == 0) return default_op;
+    
+    auto rule = rules.end();
+
+    do {
+        rule--;
+        BgpFilterOP this_op = (*rule)->apply(prefix, attribs);
+        if (this_op != NOP) return this_op;
+    } while (rule != rules.begin());
+
+    return default_op;
+}
+
+}
--- src/bgp-filter.h
+++ src/bgp-filter.h
@@ -1,17 +1,66 @@
 /**
  * @file bgp-filter.h
  * @author Nato Morichika <nat@nat.moe>
- * @brief Route filtering engine common resources.
- * @version 0.2
- * @date 2019-07-21
+ * @brief Route filtering engine.
+ * @version 0.3
+ * @date 2019-07-31
  * 
  * @copyright Copyright (c) 2019
  * 
  */
 #ifndef BGP_FILTER_H_
 #define BGP_FILTER_H_
+#include <vector>
+#include <memory>
+#include "bgp-afi.h"
+#include "prefix4.h"
+#include "prefix6.h"
+#include "bgp-path-attrib.h"
 
 namespace libbgp {
+
+/**
+ * @brief Type of filter rule.
+ * 
+ */
+enum BgpFilterRuleType {
+    F_ROUTE, /*!< Match a IP prefix */
+    F_AS_PATH, /*!< Match AS_PATH */
+    F_COMMUNITY /*!< Match COMMUNITY */
+};
+
+/**
+ * @brief Matching type of IP prefix rule.
+ * 
+ */
+enum BgpFilterRuleRouteMatchType {
+    M_EQ, /*!< Match a IP prefix equals to rule prefix */
+    M_NE, /*!< Match a IP prefix does not equals to rule prefix */
+    M_GT, /*!< Match a IP prefix that has this prefix as sub-prefix (greater then) */
+    M_LT, /*!< Match a sub-prefix of this IP prefix (less then) */
+    M_GE, /*!< Match a IP prefix equals to, or greater then the rule prefix */
+    M_LE /*!< Match a IP prefix equals to, or less then the rule prefix */
+};
+
+/**
+ * @brief Matching type of AS_PATH rule.
+ * 
+ */
+enum BgpFilterRuleAsPathMatchType {
+    M_HAS_ASN, /*!< Match routes that has the ASN in AS_PATH */
+    M_NOT_HAS_ASN, /*!< Match routes that does not have the ASN in AS_PATH */
+    M_FROM_ASN, /*!< Match routes that are originating from the ASN */
+    M_NOT_FROM_ASN /*!< Match routes that are not originating from the ASN */
+};
+
+/**
+ * @brief Matching type of COMMUNITY rule.
+ * 
+ */
+enum BgpFilterRuleCommunityMatchType {
+    M_HAS_COMMUNITY, /*!< Match routes has the given community */
+    M_NOT_HAS_COMMUNITY /*!< Match routes does not have the given community */
+};
 
 /**
  * @brief The filter operation.
@@ -24,119 +73,148 @@ enum BgpFilterOP {
 };
 
 /**
- * @brief The types of filter.
+ * @brief The base of BgpFilterRule.
  * 
  */
-enum BgpFilterType {
-    STRICT, /*!< Only match if the target subnet is an exact match */
-    LOOSE /*!< Match if the taeget subnet is a sub-prefix */
-};
-
-/**
- * @brief The Base of BgpFilterRule class.
- * 
- * @tparam T type of prefix
- */
-template <typename T> class BgpFilterRule {
+class BgpFilterRule {
 public:
-
-    /**
-     * @brief Construct a new BGP route filter rule.
-     * 
-     * @param type Type of the rule
-     * @param op Action to take
-     * @param prefix Prefix to match.
-     */
-    BgpFilterRule(BgpFilterType type, BgpFilterOP op, const T &prefix) {
-        this->type = type;
-        this->op = op;
-        this->prefix = prefix;
-    }
-
-    /**
-     * @brief Apply the rule on a prefix.
-     * 
-     * @param prefix The prefix to run this rule on.
-     * @return BgpFilterOP The operation to take.
-     */
-    BgpFilterOP apply(const T &prefix) const {
-        if (type == STRICT && this->prefix == prefix) return op;
-        if (type == LOOSE && this->prefix.includes(prefix)) return op;
-        return NOP;
-    }
-
-private:
-    BgpFilterType type;
+    uint8_t filter_type;
+    uint8_t match_type;
     BgpFilterOP op;
-    T prefix;
+
+    /**
+     * @brief Apply the rule on a route.
+     * 
+     * @param prefix Route prefix.
+     * @param attribs Path attribues.
+     * @return BgpFilterOP Action to take.
+     */
+    virtual BgpFilterOP apply(const Prefix &prefix, const std::vector<std::shared_ptr<BgpPathAttrib>> &attribs) = 0;
+    virtual ~BgpFilterRule();
 };
 
 /**
- * @brief The BgpFilterRules base class.
+ * @brief The base of of BgpFilterRuleRoute
  * 
- * @tparam T type of filtering rule.
  */
+class BgpFilterRuleRoute : public BgpFilterRule {
+public:
+    BgpFilterRuleRoute();
+
+    /**
+     * @brief Address family of this route match entry.
+     * 
+     */
+    Afi afi;
+
+    virtual ~BgpFilterRuleRoute();
+};
 
 /**
- * @brief The BgpFilterRules base class.
+ * @brief The IPv4 route filtering rule.
  * 
- * @tparam TFilter type of filtering rule.
- * @tparam TPrefix type of prefix.
  */
-template <typename TFilter, typename TPrefix> class BgpFilterRules {
+class BgpFilterRuleRoute4 : public BgpFilterRuleRoute {
 public:
-    /**
-     * @brief Construct a new BGP route filter rule list.
-     * 
-     * The default operation will be set to ACCEPT.
-     * 
-     */
-    BgpFilterRules() {
-        default_op = ACCEPT;
-    }
+    BgpFilterRuleRoute4(BgpFilterOP op, BgpFilterRuleRouteMatchType type, const Prefix4 &prefix);
 
     /**
-     * @brief Construct a new BGP route filter rule list.
+     * @brief The IPv4 prefix of this entry.
      * 
-     * @param default_op The default operation to take if non of the rules 
-     * matched the prefix.
      */
-    BgpFilterRules(BgpFilterOP default_op) {
-        this->default_op = default_op;
-    }
+    Prefix4 prefix;
+
+    BgpFilterOP apply(const Prefix &prefix, const std::vector<std::shared_ptr<BgpPathAttrib>> &attribs);
+};
+
+/**
+ * @brief The IPv6 route filtering rule.
+ * 
+ */
+class BgpFilterRuleRoute6 : public BgpFilterRuleRoute {
+public:
+    BgpFilterRuleRoute6(BgpFilterOP op, BgpFilterRuleRouteMatchType type, const Prefix6 &prefix);
 
     /**
-     * @brief Add a rule to the rules set.
+     * @brief The IPv6 prefix of this entry.
      * 
-     * @param rule The rule to add.
      */
-    void append(const TFilter &rule) {
-        rules.push_back(rule);
-    }
+    Prefix6 prefix;
+
+    BgpFilterOP apply(const Prefix &prefix, const std::vector<std::shared_ptr<BgpPathAttrib>> &attribs);
+};
+
+/**
+ * @brief The AS_PATH filtering rule
+ * 
+ */
+class BgpFilterRuleAsPath : public BgpFilterRule {
+public:
+    BgpFilterRuleAsPath(BgpFilterOP op, BgpFilterRuleAsPathMatchType type, uint32_t asn);
 
     /**
-     * @brief Apply rules set on a prefix.
+     * @brief The ASN of this entry.
      * 
-     * @param prefix The prefix to run rules set on.
-     * @return BgpFilterOP The operation to take.
      */
-    BgpFilterOP apply(const TPrefix &prefix) const {
-        if (rules.size() == 0) return default_op;
-    
-        auto rule = rules.end();
+    uint32_t asn;
 
-        do {
-            rule--;
-            BgpFilterOP this_op = rule->BgpFilterRule<TPrefix>::apply(prefix);
-            if (this_op != NOP) return this_op;
-        } while (rule != rules.begin());
+    BgpFilterOP apply(const Prefix &prefix, const std::vector<std::shared_ptr<BgpPathAttrib>> &attribs);
+};
 
-        return default_op;
+/**
+ * @brief BGP well-known communities
+ * 
+ */
+enum BgpWellKnownCommunity {
+    NO_EXPORT = 0xFFFFFF01,
+    NO_ADVERTISE = 0xFFFFFF02,
+    NO_EXPORT_SUBCONFED = 0xFFFFFF03
+};
+
+/**
+ * @brief The COMMUNITY filtering rule 
+ * 
+ */
+class BgpFilterRuleCommunity : public BgpFilterRule {
+public:
+    BgpFilterRuleCommunity(BgpFilterOP op, BgpFilterRuleCommunityMatchType type, uint16_t asn, uint16_t keyword);
+    BgpFilterRuleCommunity(BgpFilterOP op, BgpFilterRuleCommunityMatchType type, uint32_t community);
+
+    /**
+     * @brief community for this entry.
+     * 
+     */
+    uint32_t community;
+
+    BgpFilterOP apply(const Prefix &prefix, const std::vector<std::shared_ptr<BgpPathAttrib>> &attribs);
+};
+
+/**
+ * @brief The BGP filtering rules set.
+ * 
+ */
+class BgpFilterRules {
+public:
+
+    BgpFilterRules();
+    BgpFilterRules(BgpFilterOP default_op);
+
+    /**
+     * @brief Append a rule to the rule set.
+     * 
+     * @tparam T Type of this rule.
+     * @param rule The rule to append.
+     */
+    template <typename T>
+    void append(const BgpFilterRule &rule) {
+        const T &rule_typed = dynamic_cast<const T&> (rule);
+        rules.push_back(std::shared_ptr<BgpFilterRule>(new T(rule_typed)));
     }
 
+    BgpFilterOP apply(const Prefix &prefix, const std::vector<std::shared_ptr<BgpPathAttrib>> &attribs);
 private:
+    std::vector<std::shared_ptr<BgpFilterRule>> rules;
     BgpFilterOP default_op;
-    std::vector<TFilter> rules;
 };
 
 }
